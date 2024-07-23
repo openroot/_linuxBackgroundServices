863e1c7f6868028e027a38f58e10ba536187a465
Debaprasad Tapader <dev.openroot@gmail.com>
2024-07-23 22:25:35 +0530

    update

---
+++ services/usr/bin/devop_background_services/php_serve__gripRightButton55.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Name: php_serve__gripRightButton55.sh
+# Purpose: PHP Serve 'project gripRightButton55'
+# ----------------------------------------------
+
+while :
+do
+    php -S localhost:55 -t /home/devop/github/_php/gripRightButton55;
+    sleep 1;
+done
